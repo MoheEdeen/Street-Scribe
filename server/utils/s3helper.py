7d93eb81e3fb4cc38ff5f691ccb9da156e6464c4
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2025-06-01 18:03:12 -0400

    everything connection

--- server/utils/s3helper.py
+++ server/utils/s3helper.py
@@ -12,6 +12,7 @@ def upload_file_to_s3(file):
     )
     bucket_name = os.getenv("AWS_S3_BCKET_NAME")
     unique_filename = f"{uuid.uuid4()}{file.filename}"
-    s3_client.upload_fileobj(file, bucket_name, unique_filename)
+    s3_client.upload_fileobj(file, bucket_name, unique_filename, ExtraArgs={
+                             "ACL": "public-read"})
 
     return f"https://{bucket_name}.s3.{os.getenv('AWS_REGION')}.amazonaws.com/{unique_filename}"
