b44fbd91e1abe29cc4996d5697e6ed0210024a93
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2025-06-08 20:04:23 -0400

    searchbar

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, ScrollView, RefreshControl } from "react-native";
+import { StyleSheet, ScrollView, RefreshControl, FlatList } from "react-native";
 
 import Card from "@/components/Card";
 import { View } from "@/components/Themed";
 import { useEffect, useState } from "react";
-
+import { Searchbar } from "react-native-paper";
 interface Issue {
   issueType: string;
   location: string;
-  imageurl: string;
+  imageUrl: string;
 }
 
 export default function TabOneScreen() {
   const [data, setData] = useState<Issue[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [filteredData, setFilteredData] = useState(data);
+  const [searchQuery, setSearchQuery] = useState("");
   const fetchData = () => {
     setRefreshing(true);
     fetch(`localhost`)
@@ -21,7 +23,7 @@ export default function TabOneScreen() {
         const new_data = result.rows.map((item: any) => ({
           issueType: item.issue_type || "Unknown Type",
           location: item.location || "Unknown Location",
-          imageurl: item.image_url || "https://picsum.photos/700",
+          imageUrl: item.image_url || "https://picsum.photos/700",
         }));
         setData(new_data);
       })
@@ -29,28 +31,71 @@ export default function TabOneScreen() {
       .finally(() => {
         setRefreshing(false);
       });
+    setSearchQuery("");
+  };
+  const filterData = () => {
+    const filtered = data.filter((item) =>
+      item.issueType.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredData(filtered);
   };
   useEffect(() => {
-    fetchData();
+    // fetchData();
+    const dummy = [
+      {
+        issueType: "Graffiti",
+        location: "2nd St and Elm St",
+        imageUrl: "https://picsum.photos/700",
+      },
+      {
+        issueType: "Broken Streetlight",
+        location: "3rd St and Pine St",
+        imageUrl: "https://picsum.photos/700",
+      },
+      {
+        issueType: "Fallen Tree",
+        location: "4th St and Oak St",
+        imageUrl: "https://picsum.photos/700",
+      },
+    ];
+    setData(dummy);
+    setFilteredData(dummy);
   }, []);
-
+  useEffect(() => {
+    filterData();
+  }, [searchQuery, data]);
   return (
     <View style={styles.container}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+        style={{
+          marginVertical: 20,
+          backgroundColor: "transparent",
+          borderBottomWidth: 1.5,
+        }}
+      />
       <ScrollView
         style={{ flexGrow: 1 }}
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={fetchData} />
         }
       >
-        <View style={styles.separator}>
-          {data.map((item, index) => (
-            <Card
-              key={index}
-              issueType={item.issueType}
-              location={item.location}
-              imageurl={item.imageurl}
-            />
-          ))}
+        <View style={{ backgroundColor: "transparent" }}>
+          <FlatList
+            data={filteredData}
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.separator}>
+                <Card
+                  issueType={item.issueType}
+                  location={item.location}
+                  imageUrl={item.imageUrl}
+                />
+              </View>
+            )}
+          />
         </View>
       </ScrollView>
     </View>
@@ -64,7 +109,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
   },
   separator: {
-    justifyContent: "space-between",
     backgroundColor: "transparent",
     marginVertical: 20,
     gap: 50,
--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { View, TouchableOpacity } from "react-native";
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
-import { Avatar, Button, Card, Text } from "react-native-paper";
+import { Button, Card, Text } from "react-native-paper";
 // import { IconSource } from "react-native-paper/lib/typescript/components/Icon";
 import { ThemeProp } from "react-native-paper/lib/typescript/types";
 // import { ViewProps } from "react-native/Libraries/Components/View/ViewPropTypes";
 // import { ViewStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 import { styles } from "./styles";
 
-const LeftContent = (props: { size: number }) => (
-  <Avatar.Icon {...props} icon="folder" />
-);
+// const LeftContent = (props: { size: number }) => (
+//   <Avatar.Icon {...props} icon="folder" />
+// );
 
 const card = ({ issueType, location, imageurl }: any) => (
   <Card style={styles.card} theme={{ roundness: 10 } as ThemeProp}>
@@ -28,7 +28,7 @@ const card = ({ issueType, location, imageurl }: any) => (
       {/* <TouchableOpacity>
         <FontAwesome name="arrow-up" size={40} color="#1f2937" />
       </TouchableOpacity> */}
-      <Button textColor={"#ffff"} style={styles.cardButton}>
+      <Button textColor={"#ffff"} style={styles.cardButton} onPress={() => {}}>
         View
       </Button>
     </Card.Actions>
